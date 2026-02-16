4d93a8ad3a479adb663dd6bf557346b1c3161df8
Lucas Venancio <94983682+WebLucasDev@users.noreply.github.com>
2026-02-16 10:34:50 -0300

    OrderItem e OrderItemPK

--- 2 - Modelo de Domínio e ORM/dscommerce/src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
+++ 2 - Modelo de Domínio e ORM/dscommerce/src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
@@ -12,4 +12,27 @@ public class OrderItemPK {
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
+
+    public OrderItemPK() {}
+
+    public OrderItemPK(Order order, Product product) {
+        this.order = order;
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
