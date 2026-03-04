99d51c5c302a1e9522bf4c7a31f4501e9997cfb3
Lucas Venancio <94983682+WebLucasDev@users.noreply.github.com>
2026-03-04 14:23:57 -0300

    Criando excessoes

--- Projetos desenvolvidos em aula/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ Projetos desenvolvidos em aula/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.dto.CustomError;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.service.ProductService;
+import com.devsuperior.dscommerce.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.time.Instant;
 
 @RestController
 @RequestMapping(value = "/products")
